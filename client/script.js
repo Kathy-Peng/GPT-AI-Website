3e9971ece61c84f5e280372a5ffc104e2651865f
Kathy-Peng <kathypeng1999@gmail.com>
2023-06-11 18:00:45 -0400

    added client folder

---
+++ client/script.js
@@ -0,0 +1,75 @@
+import bot from '/assets/bot.svg';
+import user from '/assets/user.svg';
+
+const form = document.querySelector('form');
+const chatContainer = document.querySelector('#chat_container');
+
+let loadInterval;
+
+function loader (element){
+  element.textContent = '';
+  loadInterval = setInterval(() => {
+    element.textContent += '.';
+    if (element.textContent==="..."){
+      element.textContent = '';
+    }
+  }, 300)
+}
+
+function typeText(element, text){
+  let index= 0;
+  let interval = setInterval(()=> {
+    if (index < text.length){
+      element.innerHTML += text.charAt(index);
+      index ++;
+    }else{
+      clearInterval(interval);
+    }
+  }, 20)
+}
+
+function generateUniqueId(){
+  const timestamp = Date.now();
+  const randomNumber = Math.random();
+  const hexadecimalstring = randomNumber.toString(16);
+  return `id-${timestamp}-${hexadecimalstring}`;
+}
+
+function chatStripe(isAI, value, uniqueId){
+  return (
+    `
+    <div class='wrapper' ${isAI && 'ai'}>
+      <div class='chat'>
+        <div className='profile'>
+          <img 
+            src="${isAI? bot : user}
+            alt"${isAI?'bot':'user'}
+          />
+        </div>
+        <div class='message' id=${uniqueId}> ${value} </div>
+      </div>
+    </div>
+    `
+  )
+
+}
+
+const handleSubmit = async (e) =>{
+  e.preventDefault();
+  const data = new FormData(form);
+  chatContainer.innerHTML += chatStripe(false, data.get('prompt'));
+  form.reset();
+  const uniqueId = generateUniqueId();
+  chatContainer.innerHTML += chatStripe(true, " ", uniqueId);
+  chatContainer.scrollTop = chatContainer.scrollHeight;
+  const messageDiv = document.getElementById(uniqueId);
+  loader(messageDiv);
+
+}
+
+form.addEventListener('submit', handleSubmit);
+form.addEventListener('keyup', (e) => {
+  if (e.keyCode===13){
+    handleSubmit(e);
+  }
+})
